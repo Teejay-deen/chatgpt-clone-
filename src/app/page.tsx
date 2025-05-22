ed488e5f8478c308ef7dc0860f2b85132150e151
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-05-22 09:55:52 +0100

    added the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-red-400">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#1a1a1a] text-white">
+      Home page
     </div>
   );
 }
