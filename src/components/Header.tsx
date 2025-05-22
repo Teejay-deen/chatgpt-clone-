9c89c84cc70b9476c9c65f9ff1c3283fa3463d74
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-05-22 09:27:49 +0100

    added fortress logo button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ import { IoChevronDown } from "react-icons/io5";
 
 const Header = () => {
   return (
-    <div className='flex justify-between i  tems-center m-2.5 h-10 top-0 left-0 pl-2 pr-12 absolute w-full'>
-        <button>FORTRESS CHATGPT<IoChevronDown /></button>
+    <div className='flex justify-between items-center m-2.5 h-10 top-0 left-0 pl-2 pr-12 absolute w-full'>
+        <button className='flex items-center hover:bg-black font-semibold tracking-wide px-3 rounded-lg py-2 gap-2 bg-[#2f2f2f]'>FORTRESS CHATGPT<IoChevronDown /></button>
         <Link href={"/sigin"} >Sign In</Link>
     </div>
   )
