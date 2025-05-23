55e50f85c20459da5f1c444bd7960973dd963281
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-05-23 15:52:47 +0100

    added the chat input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+// import ChatInput from "@/components/InputInput";
+import ChatHelp from "@/components/chatHelp";
+import ChatInput from "@/components/chatInput";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#1a1a1a] text-white">
-      Home page
-    </div>
+    <main className="min-h-screen flex flex-col px-2 items-center justify-center">
+      <div className=" w-full mx-auto flex flex-col items-center gap-5">
+        <h2 className="text-xl md:text-3xl font-semibold px-4 text-white">
+        What can i help you with?
+         </h2>
+
+       {/* chat input */}
+       <ChatInput />
+       <ChatHelp />
+      </div>
+    
+    </main>
   );
 }
---
+++ src/components/chatInput.tsx
@@ -0,0 +1,26 @@
+
+"use client";
+import React, { useState } from 'react'
+import { ImArrowUpRight2 } from 'react-icons/im'
+import { TbPaperclip } from 'react-icons/tb'
+
+const ChatInput = () => {
+
+    const [prompt , setPrompt] = useState("")
+  return (
+    <div className='w-full flex flex-col gap-2 justify-center items-center max-w-3xl px-4  mx-auto pt-3'>
+        <form className='bg-white/10 rounded-full flex items-center py-2.5 px-4 w-full'>
+             <TbPaperclip className='text-2xl -rotate-45 text-white' />
+            <input onChange={(e)=> setPrompt(e.target.value)} value={prompt} className='text-white px-3 outline-none font-medium tracking-wide w-full placeholder:text-gray-400 bg-transparent ' type="text" placeholder='Message Fortress ChatGPT' name="" id="" />
+            <button disabled={!prompt} className='rounded-full p-2.5 text-black bg-white disabled:bg-white/30'>
+                <ImArrowUpRight2 className='text-sm -rotate-45 text-black/80' />
+            </button>
+        </form>
+        <p className='text-sm font-medium text-gray-400'>Fortress ChatGPT can make mistake. Check the important Info.</p>
+        {/* model selection for smaller screen */}
+
+    </div>
+  )
+}
+
+export default ChatInput
