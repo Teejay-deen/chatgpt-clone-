da5549774a425be1bae59597b99f84eec09bbc21
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-06-17 08:21:23 +0100

    fixes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import NewChat from './NewChat'
+import Link from 'next/link'
+import { IoHome } from 'react-icons/io5'
 
 const SideBar = () => {
   return (
-    <div>
-        Sidebar
+    <div className='hidden md:flex flex-col h-screen relative p-2.5 '>
+       <div className='flex items-center gap-1'>
+         <Link className='border border-white/20 rounded-md text-white/50 hover:border-white/50 hover:text-white text-xs md:text-base p-1.5 md:p-2' href={"/"}>
+         <IoHome /> 
+        </Link>
+        <NewChat />
+       </div>
     </div>
   )
 }
