a5bc947a453e3650b9d7eb2fa89528517cac9f9f
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-06-17 08:22:09 +0100

    New chats component

---
+++ src/components/NewChat.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { FaPlus } from 'react-icons/fa'
+
+
+const NewChat = () => {
+    const router =useRouter()
+    const createNewChat =async()=>{
+        //creating a new chats id in firestore
+        router.push("/chat/676767")
+    }
+  return (
+    <button onClick={createNewChat} className='flex items-center justify-center gap-2 border border-white/20 rounded-md text-white/50 hover:border-white/50 w-full hover:text-white text-xs md:text-base px-2 py-1'> <FaPlus /> New Chat</button>
+  )
+}
+
+export default NewChat
